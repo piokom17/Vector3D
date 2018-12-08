7f98285ebe4ef6400a534fafc67868a889ab844f
Piotr Komar <komarenko17@gmail.com>
2018-12-08 12:43:42 +0100

    First Commit Added bVector3D class

---
+++ Vector3D.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Vector3D.h"
+
+
+Vector3D::Vector3D()
+{
+}
+
+
+Vector3D::~Vector3D()
+{
+}
---
+++ Vector3D.h
@@ -0,0 +1,8 @@
+#pragma once
+class Vector3D
+{
+public:
+	Vector3D();
+	~Vector3D();
+};
+
